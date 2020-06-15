f9879af5814a344a09debdb1312b0f53fce26a42
Taras Honcharuk <taras.goncharuk22@gmail.com>
2020-06-15 17:33:18 +0300

    html css

---
+++ script.js
@@ -0,0 +1,8 @@
+// const images = document.getElementById('images');
+//
+//
+// function addPicture() {
+//     fetch('https://boiling-refuge-66454.herokuapp.com/images')
+//         .then(res => res.json())
+//         .
+// }
