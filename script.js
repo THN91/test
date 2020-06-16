24847a9408c62250fe63e794212ea7da5a3f3b80
Taras Honcharuk <taras.goncharuk22@gmail.com>
2020-06-16 16:14:27 +0300

    js

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 const images = document.getElementById('images');
+const modal = document.getElementById('modal');
+const closeBtn = document.getElementById('close-btn')
 
+
+function getID(e) {
+    if (e.target.classList.contains('img')) {
+        const imgID = e.target.id;
+
+        showModal(imgID);
+    }
+}
+
+function showModal(imgID) {
+    modal.classList.add('show')
+}
+
+function closeModal(e) {
+    console.log(e)
+    modal.classList.remove('show')
+
+    addImages();
+}
 
 function addImages() {
     fetch('https://boiling-refuge-66454.herokuapp.com/images')
         .then(res => res.json())
         .then(data => {
 
-            const pictures = data.map(index => index.url);
+            const pictures = data.map(item => item.url);
+            const imgID = data.map(item => item.id);
 
             for (let i = 0; i <= 10; i++) {
                 if (pictures[i] !== undefined) {
-                    console.log(pictures[i]);
                     pic = document.createElement('img');
                     pic.className = 'img';
+                    pic.id = `${imgID[i]}`;
                     pic.src = `${pictures[i]}`;
                     images.appendChild(pic)
                 } else {
@@ -23,3 +45,6 @@ function addImages() {
 }
 
 addImages();
+
+images.addEventListener('click', getID);
+closeBtn.addEventListener("click", closeModal);
